334aebe726f1f05ceabc86c1d271919609c1a7e7
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-06-14 14:40:48 +0100

    [Jade|Trystan] Create the PowerRangerColorHeader component and renders

---
+++ src/Components/PowerRangerColorHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PowerRangerColorHeader extends React.Component {
+    render() {
+        return (
+          <h1>Power Ranger Color</h1>
+        );
+    }
+}
+
+export default PowerRangerColorHeader;
